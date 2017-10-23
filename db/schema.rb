ce6d8d8132bdf18b59f0e7d3d6b01b3c4d98c74c
Nacho <nacho.labmar@gmail.com>
2017-10-23 16:51:58 -0300

    minor fix to add ingredients html

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171023180950) do
+ActiveRecord::Schema.define(version: 20171023191053) do
 
   create_table "formulation_ingredients", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "formulation_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20171023180950) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "tags"
   end
 
   create_table "patients", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
