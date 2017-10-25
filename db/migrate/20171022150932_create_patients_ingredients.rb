59841d93314e63a99ea27aabb1e244f803f16be9
Nacho <nacho.labmar@gmail.com>
2017-10-25 14:11:24 -0300

    Migration does not work on Postgre

--- db/migrate/20171022150932_create_patients_ingredients.rb
+++ db/migrate/20171022150932_create_patients_ingredients.rb
@@ -2,7 +2,7 @@ class CreatePatientsIngredients < ActiveRecord::Migration[5.1]
   def change
     create_table :patients_ingredients do |t|
       t.references :patient, foreign_key: true
-      t.references :ingridient, foreign_key: true
+      t.references :ingredient, foreign_key: true
       t.integer :percentage
 
       t.timestamps
--- db/migrate/20171023171003_fix_column_name.rb
+++
@@ -1,5 +0,0 @@
-class FixColumnName < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :patients_ingredients, :ingridient_id, :ingredient_id
-  end
-end
